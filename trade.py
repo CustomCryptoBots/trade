4b27f0db85be6ee02df74e767dc9bfca829e7af9
Custom Crypto Bots <customcryptobots@gmail.com>
2024-05-30 14:09:26 -0500

    input file and detect new cryptos

--- trade.py
+++ trade.py
@@ -1,49 +1,40 @@
-# deprecated code workaround
 import collections
 from collections import abc
 collections.MutableMapping = abc.MutableMapping
 collections.Mapping = abc.Mapping
 collections.Sequence = abc.Sequence
 collections.Callable = abc.Callable
-
-# imports
 from coinbase.wallet.client import Client as cbc
 import cbpro
 import time
 from datetime import datetime
+import csv
 
 
-class trade:
-    
-    
-    # class variables
-    api = open("/home/dev/code/dat/api.txt", "r").read().splitlines()
-    cb_client = cbc(api[0], api[1])
-    cbp = cbpro.PublicClient()
-    wirePath = "/home/dev/code/tmp/" + str(datetime.now().strftime("%Y-%m-%d %H-%M-%S")) + ".txt"   
-    names = []
-    
-    
-    # initializer
-    def __init__(self):
+class Trade:
+    def __init__(self):  
         start_time = time.time()    # track runtime
+        
+        # instance variables
+        self.api = open("/home/dev/code/dat/api.txt", "r").read().splitlines()  
+        self.wirePath = "/home/dev/code/tmp/" + str(datetime.now().strftime("%Y-%m-%d %H-%M-%S")) + ".txt"
+        self.cb_client = cbc(self.api[0], self.api[1])      # coinbase api connection
+        self.cbp = cbpro.PublicClient()                     # live price data
+        self.include = []                                   # available assets
+
+        self.syncWallets()          # match input file with coinbase api
     
-        self.getWallets()           # populate names[]
-    
-        # check price to identify viable assets
-        invalid = 0
-        for n in self.names:
-            self.output(n + " ")
+        # asset prices
+        prices = []
+        for n in self.include:
             try:
                 ticker = self.getPrice(n)
-                self.output(str(ticker['price']) + "\n")                
-                                
+                prices.append(n + " " + str(ticker['price']))                
             except:
-                invalid += 1
-                self.output("invalid\n")
-
-        self.output("\n" + str(invalid) + " invalid wallets\n\n")                     
+                self.output(self.output(n + " " + str(e) + "\n\n"))
         
+        self.output("Prices\n" + str(prices) + "\n\n")
+
         end_time = time.time()
         sec = end_time - start_time
         self.output("\nRuntime\n")
@@ -53,33 +44,49 @@ class trade:
     
     # streamline output
     def output(self, message):
-        wire = open(self.wirePath, "a")
-        wire.write(message)
-        wire.close()
+        with open(self.wirePath, "a") as wire:
+            wire.write(message)
 
 
-    # get all coinbase wallet names
-    def getWallets(self):
+    # match known assets with all available
+    # via api key and check for new wallets
+    def syncWallets(self):
+        exclude = []
+        
+        # read from input file
+        with open("/home/dev/code/dat/inp.txt", "r") as infile:
+            reader = csv.reader(infile)
+            for row in reader:
+                if row[1] == "0":           
+                    self.include.append(row[0])
+                elif row[1] == "1":
+                    exclude.append(row[0])
+        
+        # get all wallets from coinbase api
         try:
             account = self.cb_client.get_accounts(limit = 300)
-
         except Exception as e:
             self.output(str(e) + "\n\n")
-    
-        # populate names class variable
-        self.names = [wallet['name'].replace(" Wallet", "") for wallet in account.data]
-            
-    
-    # check live price
+   
+        # check for new assets
+        names = [wallet['name'].replace(" Wallet", "") for wallet in account.data]
+        for n in names:
+            if n not in self.include and n not in exclude:
+                self.output("### NEW CRYPTO -> " + n + " ###\n\n") 
+        
+        self.output("Exclude\n" + str(exclude) + "\n\n")
+
+
+    # live price
     def getPrice(self, asset):
         try:
             asset += "-USD"
             ticker = self.cbp.get_product_ticker(product_id = asset)
-            return ticker
-        
+            return ticker    
         except Exception as e:
             self.output(str(e) + "\n\n")
+            return None
 
 
 # void main
-_new = trade()
+_new = Trade()
