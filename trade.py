e0dbcb5a38c09db647dea9ced4a57129dbc4175c
Custom Crypto Bots <customcryptobots@gmail.com>
2024-06-03 18:54:39 -0500

    adios cbpro and batching for optimized runtime

--- trade.py
+++ trade.py
@@ -1,58 +1,34 @@
-import collections
-from collections import abc
-collections.MutableMapping = abc.MutableMapping
-collections.Mapping = abc.Mapping
-collections.Sequence = abc.Sequence
-collections.Callable = abc.Callable
 from coinbase.wallet.client import Client as cbc
-import cbpro
 import time
 from datetime import datetime
 import csv
 import pandas as pd
+import requests
+from concurrent.futures import ThreadPoolExecutor
 
 
 class Trade:
     def __init__(self):  
-        start_time = time.time()    # track runtime
+        start_time = time.time()                            # track runtime
         
         # instance variables
         with open("/home/dev/code/dat/api.txt", "r") as api_file:
             self.api = api_file.read().splitlines()
         self.wirePath = "/home/dev/code/tmp/" + str(datetime.now().strftime("%Y-%m-%d %H-%M-%S")) + ".txt"
         self.cb_client = cbc(self.api[0], self.api[1])      # coinbase api connection
-        self.cbp = cbpro.PublicClient()                     # live price data
         self.include = set()                                # available assets
         self.output_buffer = []                             # for all output
-        
-        self.syncWallets()                                  # match input file with coinbase api
-    
-        # asset prices and historical trading data
         self.output_buffer.append(f"name,mean,median,mode,std,var,min,max,range,iqr,vol,days\n")
-        prices = set()    
-        for n in self.include:
-            try:
-                ticker = self.getPrice(n)
-                #self.output_buffer.append(f"{n}\n{ticker}\n\n")
-                if ticker:                                  # get price, candlestick and stats info
-                    prices.add(f"{n} {ticker}")                
-                    df = self.getHistoric(n)
-                    stats = self.timeDiff(df) 
-                    mean = stats['mean'] 
-                    median = stats['median']
-                    mode = stats['mode']
-                    std = stats['std']
-                    var = stats['variance']
-                    minn = stats['min']
-                    maxx = stats['max']
-                    rnge = stats['range'] 
-                    iqr = stats['iqr']
-                    vol = round(sum(df['Volume']), 0)
-                    length = round((df['Date'].max() - df['Date'].min()).total_seconds() / (24 * 3600), 2)      # convert to days
-                    self.output_buffer.append(f"{n},{mean},{median},{mode},{std},{var},{minn},{maxx},{rnge},{iqr},{vol},{length}\n")
-            except Exception as e:
-                self.output_buffer.append(f"{e}\n\n")
-        #self.output_buffer.append(f"Include {len(self.include)}\n{prices}\n\n")
+        
+        # sync wallets then batch process candlestick data api calls
+        self.syncWallets()
+        with ThreadPoolExecutor(max_workers = 10) as executor:
+            futures = [executor.submit(self.getHistoric, asset) for asset in self.include]
+            for future in futures:
+                asset, df = future.result()
+                if df is not None:
+                    self.timeDiff(asset, df)
+        
         end_time = time.time()
         sec = end_time - start_time
         self.output_buffer.append(f"\nRuntime\n")
@@ -84,7 +60,7 @@ class Trade:
         try:
             account = self.cb_client.get_accounts(limit = 300)
         except Exception as e:
-            #self.output_buffer.append(f"{str(e)}\n\n")
+            self.output_buffer.append(f"{str(e)}\n\n")
             return
         
         # check for new/removed assets
@@ -96,7 +72,6 @@ class Trade:
             self.output_buffer.extend([f"### NEW CRYPTO -> {n} ###\n\n" for n in new_cryptos])
         if removed_cryptos:
             self.output_buffer.extend([f"### REMOVED CRYPTO -> {n} ###\n\n" for n in removed_cryptos])
-        #self.output_buffer.append(f"Exclude {len(exclude)}\n{exclude}\n\n")
 
     
     # authenticated price check
@@ -106,7 +81,7 @@ class Trade:
             price = self.cb_client.get_spot_price(currency_pair = currency_pair)
             return price['amount']
         except Exception as e:
-            #self.output_buffer.append(f"{asset}\n{e}\n\n")
+            self.output_buffer.append(f"{asset}\n{e}\n\n")
             return None
 
 
@@ -114,25 +89,28 @@ class Trade:
     def getHistoric(self, asset):
         try:
             assetUSD = asset + "-USD"
-            # 15 minute granularity * 300 rows = 3.125 days
-            raw = self.cbp.get_product_historic_rates(product_id = assetUSD, granularity = 900)
+            url = f"https://api.pro.coinbase.com/products/{assetUSD}/candles"
+            
+            # send get request to the api
+            response = requests.get(url, params = {'granularity': 900})
+            response.raise_for_status()                             # exception for HTTP errors
+            raw = response.json()
             df = pd.DataFrame(raw, columns = ['Date', 'Low', 'High', 'Open', 'Close', 'Volume'])
-            df = df.iloc[::-1].reset_index(drop = True)               # chronological order
-            df['Date'] = pd.to_datetime(df['Date'], unit = 's')       # make readable
-            return df
-        except Exception as e:
-            #self.output_buffer.append(f"{str(e)}\n\n")
+            df = df.iloc[::-1].reset_index(drop = True)             # chronological order
+            df['Date'] = pd.to_datetime(df['Date'], unit = 's')     # make date readable
+            return asset, df
+        except requests.exceptions.RequestException as e:
+            self.output_buffer.append(f"{str(e)}\n\n")
             return None
 
 
     # analyze time difference reporting issue
-    def timeDiff(self, df):
+    def timeDiff(self, asset, df):
         # in minutes between consecutive rows
         df['TimeDiff'] = (df['Date'] - df['Date'].shift(1)).dt.total_seconds() / 60.0
-        
         # drop the first row because it has NaN in 'TimeDiff'
-        df = df.iloc[1:].reset_index(drop=True)
-        
+        df = df.iloc[1:].reset_index(drop = True)
+
         # calculate statistical information
         stats = {
             'mean': round(df['TimeDiff'].mean(), 2),
@@ -143,10 +121,12 @@ class Trade:
             'min': df['TimeDiff'].min(),
             'max': df['TimeDiff'].max(),
             'range': df['TimeDiff'].max() - df['TimeDiff'].min(),
-            'iqr': round(df['TimeDiff'].quantile(0.75) - df['TimeDiff'].quantile(0.25), 2)
+            'iqr': round(df['TimeDiff'].quantile(0.75) - df['TimeDiff'].quantile(0.25), 2),
+            'vol': round(sum(df['Volume']), 0),
+            'length': round((df['Date'].max() - df['Date'].min()).total_seconds() / (24 * 3600), 2)  # days
         }
 
-        return stats
+        self.output_buffer.append(f"{asset},{stats['mean']},{stats['median']},{stats['mode']},{stats['std']},{stats['variance']},{stats['min']},{stats['max']},{stats['range']},{stats['iqr']},{stats['vol']},{stats['length']}\n")
 
 
 # void main
